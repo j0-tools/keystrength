b1c278a72ad90facffa670f175b374fb93df6523
Josh Birch <2312292@chester.ac.uk>
2024-09-05 11:28:02 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from tkinter import font as tkfont
+import ttkbootstrap as tkb
+
+
+
+###################
+### KEYSTRENGTH ### 
+###################
+
+
+
+### CONSOLE TEST ###
+
+
+
+
+
+
