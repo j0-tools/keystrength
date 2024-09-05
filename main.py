e8b793cae16abee12144bda56f7670d422212950
Josh Birch <2312292@chester.ac.uk>
2024-09-05 11:53:18 +0100

    Outline basic CLI for password length check
    Outlines (very) basic control flow for password checks and builds foundation for checking the length of a password.

--- main.py
+++ main.py
@@ -10,7 +10,46 @@ import ttkbootstrap as tkb
 
 
 
-### CONSOLE TEST ###
+### CLI FIRST RUN ###
+
+# Welcome message 
+print("\n\n### KeyStrength Password Checker ###")
+
+value = True
+
+# Main loop
+while value == True:
+    
+    # Prompt for password
+    pw = input("\nType a password: ")
+    length = len(pw)
+    
+    # Check if password empty
+    if pw == "":
+        print("Password cannot be empty.\n")
+        continue
+    
+    # Length check
+    if length < 8:
+        print("Password is very weak.\nPassword needs more characters.\n")
+    elif length < 12:
+        print("Password is weak.\nPassword needs more characters.\n")
+    elif length < 16:
+        print("Password is moderate.\nTo strengthen, try using a password more than 15 characters in length.\n")
+    else:
+        print("Password is strong.\nWell done!\n")
+
+    # Continue?
+    yn = input("Try another password? (Y/N): ")
+    if yn.upper() == "Y":
+        continue
+    else:
+        break
+
+print("\nThanks for using KeyStrength!")
+
+
+
 
 
 
