fd9ac63a92244586ea29504c6485e0848df22288
Josh Birch <2312292@chester.ac.uk>
2024-09-17 15:15:04 +0100

    Adjust length_diversity function in test for better granularity/brevity of code
    Working, other functions still need adjusting

--- tests.py
+++ tests.py
@@ -8,190 +8,32 @@ import math
 class Password():
     def __init__(self, password):
         self.password = password
-        self.score = 100
+        self.score = 0
 
 
 
     # ### CHECKS ###
 
-    # # Length check (WORKING)
-    # def length_check(self):
-    #     length = len(self.password)
-    #     if length < 8:
-    #         return -30
-    #     elif length < 11:
-    #         return 0
-    #     elif length < 13:
-    #         return 4
-    #     elif length < 18:
-    #         return 8
-    #     else:
-    #         return 34
     
-    # # Diversity check (WORKING)
-    # def diversity_check(self):
-
-    #     if len (self.password) >= 18:
-    #         return 11
-
-    #     else:
-    #         categories = 0
-    #         score = 0
-
-    #         if any(c.islower() for c in self.password):
-    #             categories += 1
-    #         if any(c.isupper() for c in self.password):
-    #             categories += 1
-    #         if any(c.isdigit() for c in self.password):
-    #             categories += 1
-    #         if any(c in '''!@#$%^&*()-_=+{}[];:"\'<>,.?/''' for c in self.password):
-    #             categories += 1
-
-    #         score = categories * 3
-            
-    #         if categories == 4:
-    #             score += 10
-
-    #         if categories < 4 and len(self.password) < 15:
-    #             score -= 11 
-
-    #         if categories <= 1:
-    #             score -= 15
-
-    #         return score
-    
-
     # Combined length and diversity check 
     def length_diversity(self):
+
+        password = self.password
         length = len(self.password)
 
-        if length < 8:
+        categories = sum([
+            any(c.islower() for c in password),
+            any(c.isupper() for c in password),
+            any(c.isdigit() for c in password),
+            any(c in '''!@#$%^&*()-_=+{}[];:"\'<>,.?/''' for c in password)
+        ])
+
+        if length < 8: 
             return -99
+        base = length * 2
+        bonus = categories * 10
 
-        categories = 0
-
-        has_lower = any(c.islower() for c in self.password)
-        has_upper = any(c.isupper() for c in self.password)
-        has_digits = any(c.isdigit() for c in self.password)
-        has_symbols = any(c in '''!@#$%^&*()-_=+{}[];:"\'<>,.?/''' for c in self.password)
-
-
-        # 1st column
-        if has_digits and not (has_lower or has_upper or has_symbols):    # Numbers only
-            if length <= 10:
-                return -99
-            elif length <= 15:
-                return -79
-            else:
-                return -59
-        
-
-        # 2nd column
-        elif has_lower and not (has_upper or has_digits or has_symbols):    # Lower only 
-            if length <= 10:
-                return -79
-            elif length <= 13:
-                return -59
-            elif length <= 17:
-                -39
-            else:
-                return 0
-
-        elif has_upper and not (has_lower or has_digits or has_symbols):    # Upper only 
-            if length <= 10:
-                return -79
-            elif length <= 13:
-                return -59
-            elif length <= 17:
-                -39
-            else:
-                return 0
-        
-
-        # 3rd column
-        elif has_lower and has_upper and not (has_digits or has_symbols):    # Upper and lower
-            if length <= 9:
-                return -79
-            elif length <= 11:
-                return -59
-            elif length <= 14:
-                return -39
-            else:
-                return 0
-
-        elif has_digits and has_symbols and not (has_lower or has_upper):    # Digits and symbols
-            if length <= 9:
-                return -79
-            elif length <= 11:
-                return -59
-            elif length <= 14:
-                return -39
-            else:
-                return 0
-
-        elif has_lower and has_symbols and not (has_upper or has_digits):    # Lower and symbols
-            if length <= 9:
-                return -79
-            elif length <= 11:
-                return -59
-            elif length <= 14:
-                return -39
-            else:
-                return 0
-
-        elif has_upper and has_symbols and not (has_lower or has_digits):    # Upper and symbols
-            if length <= 9:
-                return -79
-            elif length <= 11:
-                return -59
-            elif length <= 14:
-                return -39
-            else:
-                return 0
-
-
-        # 4th column
-        elif has_lower and has_upper and has_symbols and not has_digits:    # Lower, upper and symbols
-            if length <= 10:
-                return -59
-            elif length <= 13:
-                return -39
-            else:
-                return 0
-
-        elif has_lower and has_digits and has_symbols and not has_upper:    # Lower, digit and symbol
-            if length <= 10:
-                return -59
-            elif length <= 13:
-                return -39
-            else:
-                return 0
-
-        elif has_upper and has_digits and has_symbols and not has_lower:    # Upper, digit and symbol
-            if length <= 10:
-                return -59
-            elif length <= 13:
-                return -39
-            else:
-                return 0
-
-        elif has_digits and has_lower and has_upper and not has_symbols:    # Digit, upper, and lower
-            if length <= 10:
-                return -59
-            elif length <= 13:
-                return -39
-            else:
-                return 0
-        
-
-        # 5th column
-        elif has_digits and has_lower and has_upper and has_symbols:    # All
-            if length <= 10:
-                return -59
-            if length <= 12:
-                return -39
-            else:
-                return 0
+        return base + bonus
 
 
     # Common passwords (WORKING)
@@ -204,90 +46,90 @@ class Password():
         password_lower = self.password.lower()
 
         if password_lower in common_passwords_lower:
-            return -100
+            return -99
         else:
             return 6
     
-    # # Personal info (names, movies, etc) (WORKING)
-    # def common_names_check(self):
-    #     if len (self.password) >= 18:
-    #         return 11
-    #     else:
-    #         with open("data/names.json", "r") as file:
-    #             common_names = json.load(file)
+    # Personal info (names, movies, etc) (WORKING)
+    def common_names_check(self):
+        if len (self.password) >= 19:
+            return 12
+        else:
+            with open("data/names.json", "r") as file:
+                common_names = json.load(file)
             
-    #         password_lower = self.password.lower()
+            password_lower = self.password.lower()
             
-    #         if any(name.lower() in password_lower for name in common_names):
-    #             return -8
-    #         else:
-    #             return 6
+            if any(name.lower() in password_lower for name in common_names):
+                return -22
+            else:
+                return 6
 
-    # # Dictionary words (WORKING)
-    # def dictionary_words_check(self):
-    #     if len (self.password) >= 18:
-    #         return 11
-    #     else:
-    #         with open("data/dictionarywords.json", "r") as file:
-    #             common_words = json.load(file)
+    # Dictionary words (WORKING)
+    def dictionary_words_check(self):
+        if len (self.password) >= 19:
+            return 12
+        else:
+            with open("data/dictionarywords.json", "r") as file:
+                common_words = json.load(file)
 
-    #         longer_words = [word for word in common_words if len(word) >= 3]
+            longer_words = [word for word in common_words if len(word) >= 3]
             
-    #         if any(word in self.password for word in longer_words):
-    #             return -6
-    #         else:
-    #             return 6
+            if any(word in self.password for word in longer_words):
+                return -22
+            else:
+                return 6
 
-    # # Repeated characters (WORKING)
-    # def repeat_check(self):
+    # Repeated characters (WORKING)
+    def repeat_check(self):
         
-    #     password_lower = self.password.lower()
-    #     common = [
-    #     # Alphabetic
-    #     "abc", "bcd", "cde", "def", "efg", "fgh", "ghi", "hij",
-    #     "ijk", "jkl", "klm", "lmn", "mno", "nop", "opq", "pqr",
-    #     "qrs", "rst", "stu", "tuv", "uvw", "vwx", "wxy", "xyz",
-    #     # Reverse
-    #     "zyx", "yxw", "xwv", "wvu", "vut", "uts",
-    #     # Numeric
-    #     "123", "234", "345", "456", "567", "678", "789", "890",
-    #     # Reverse numeric
-    #     "987", "876", "765", "654", "543", "432", "321",
-    #     # Keyboard patterns
-    #     "qwe", "wer", "ert", "rty", "tyu", "yui", "uio", "iop",
-    #     "asd", "sdf", "dfg", "fgh", "ghj", "hjk", "jkl",
-    #     "zxc", "xcv", "cvb", "vbn", "bnm",
-    #     # Keyboard patterns (AZERTY)
-    #     "aze", "zer", "ert", "rty", "tyu", "yui", "uio", "iop",
-    #     "qsd", "sdf", "dfg", "fgh", "ghj", "hjk", "jkl", "lm",
-    #     "wxc", "xcv", "cvb", "vbn",
-    #     # Common substitutions
-    #     "!@#", "@#$", "#$%", "$%^", "%^&", "^&*", "&*(", "*()"]
+        password_lower = self.password.lower()
+        common = [
+        # Alphabetic
+        "abc", "bcd", "cde", "def", "efg", "fgh", "ghi", "hij",
+        "ijk", "jkl", "klm", "lmn", "mno", "nop", "opq", "pqr",
+        "qrs", "rst", "stu", "tuv", "uvw", "vwx", "wxy", "xyz",
+        # Reverse
+        "zyx", "yxw", "xwv", "wvu", "vut", "uts",
+        # Numeric
+        "123", "234", "345", "456", "567", "678", "789", "890",
+        # Reverse numeric
+        "987", "876", "765", "654", "543", "432", "321",
+        # Keyboard patterns
+        "qwe", "wer", "ert", "rty", "tyu", "yui", "uio", "iop",
+        "asd", "sdf", "dfg", "fgh", "ghj", "hjk", "jkl",
+        "zxc", "xcv", "cvb", "vbn", "bnm",
+        # Keyboard patterns (AZERTY)
+        "aze", "zer", "ert", "rty", "tyu", "yui", "uio", "iop",
+        "qsd", "sdf", "dfg", "fgh", "ghj", "hjk", "jkl", "lm",
+        "wxc", "xcv", "cvb", "vbn",
+        # Common substitutions
+        "!@#", "@#$", "#$%", "$%^", "%^&", "^&*", "&*(", "*()"]
 
-    #     if any(password_lower.count(c) > 2 for c in set(password_lower)):
-    #         return -8
-    #     elif any(seq in password_lower for seq in common):
-    #         return -8
-    #     else:
-    #         return 11
+        if any(password_lower.count(c) > 2 for c in set(password_lower)):
+            return -12
+        elif any(seq in password_lower for seq in common):
+            return -12
+        else:
+            return 6
         
-    # # Entropy (randomness)
-    # def entropy_check(self):
-    #     char_set = set(self.password)
-    #     entropy = len(self.password) * math.log2(len(char_set))
+    # Entropy (randomness)
+    def entropy_check(self):
+        char_set = set(self.password)
+        entropy = len(self.password) * math.log2(len(char_set))
 
-    #     if entropy < 5:
-    #         return -50
-    #     elif entropy < 25:
-    #         return -10
-    #     elif entropy < 30:
-    #         return 0
-    #     elif entropy < 45:
-    #         return 5
-    #     elif entropy < 65:
-    #         return 10
-    #     else:
-    #         return 20
+        if entropy < 5:
+            return -98
+        elif entropy < 25:
+            return -60
+        elif entropy < 30:
+            return 0
+        elif entropy < 45:
+            return 8
+        elif entropy < 65:
+            return 14
+        else:
+            return 23
 
 
 
@@ -295,16 +137,15 @@ class Password():
 
     def strength(self):
         self.score = (
-            self.length_check() +
-            self.diversity_check() +
+            self.length_diversity() +
             self.common_passwords_check() +
             self.common_names_check() +
             self.dictionary_words_check() +
             self.repeat_check() +
             self.entropy_check() 
         )
-        if len(self.password) > 30:
-            self.score += 5
+        if len(self.password) > 20:
+            self.score += 10
         return max(1, min(100, self.score))    # 1-100
 
 
